750897099645d5c9df5669fa81a225e8789ae5b1
Zelak7561 <bs0057082@gmail.com>
2024-10-28 22:51:06 +0100

    EXERCISE FINISHED

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdio.h>
+
+void somSegniAlterni(int nTotale , float *sumSegniAlterni);
+
+int main(void) {
+
+    int nTotale;
+    float sumSegniAlterni;
+
+    printf("Inserisci il numero totale: \n");
+    scanf("%d", &nTotale);
+
+    somSegniAlterni(nTotale, &sumSegniAlterni);
+
+    printf("SumSegniAlterni: %f\n", sumSegniAlterni);
+
+    return 0;
+}
+
+void somSegniAlterni(int nTotale, float *sumSegniAlterni) {
+    for (int i = 0; i < nTotale; i++) {
+        float n = pow(-1, i);
+        float d = 2.0 * i + 1.0;
+         *sumSegniAlterni = *sumSegniAlterni + n/d;
+    }
+}
